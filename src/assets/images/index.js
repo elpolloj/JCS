c71c7824c33e49b2dd4db20c034fe86ea62ce7cb
elpolloj <triplejjarrah@gmail.com>
2024-07-04 20:45:23 +0300

    Initial commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,46 @@
+import bigDesktop1 from './big-desktop1.png'
+import bigPOS from './big-pos1.png'
+import bigPrinter from './big-printer.png'
+
+import customer1 from './customer1.jpeg'
+import customer2 from './customer2.svg'
+
+import footerLogo from './footer-logo.svg'
+import headerLogo from './header-logo.svg'
+
+import offer from './offer.svg'
+
+import laptop1 from './laptop1.svg'
+import laptop2 from './laptop2.svg'
+import minipc from './minipc.svg'
+import scanner from './scanner.svg'
+import bigpos from './bigpos.svg'
+
+import thumbnailBackground from './thumbnail-bg.svg'
+import thumbnailDesktop1 from './thumbnail-desktop1.svg'
+import thumbnailPos from './thumbnail-pos1.svg'
+import thumbnailPrinter from './thumbnail-printer.svg'
+
+export {
+    bigDesktop1,
+    bigPOS,
+    bigPrinter,
+
+    customer1,
+    customer2,
+
+    footerLogo,
+    headerLogo,
+
+    offer,
+    laptop1,
+    laptop2,
+    minipc,
+    scanner,
+    bigpos,
+
+    thumbnailBackground,
+    thumbnailDesktop1,
+    thumbnailPos,
+    thumbnailPrinter
+}
